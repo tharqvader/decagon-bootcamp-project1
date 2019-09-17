073598d6f0f1bab8bfae848e7e5edd71cfc0698f
tharqvader <udokwu.chika@gmail.com>
2019-09-17 12:34:58 +0100

    finished with db connection to reg form

--- js/index.js
+++ js/index.js
@@ -4,40 +4,47 @@ $(document).ready(function() {
         const firstname = $('#firstname').val();
         const lastname = $('#lastname').val();
         const username = $('#username').val();
-        const email = $('#email').val();
         const password = $('#password').val();
-
-        if (!firstname || !lastname || !username || !email || !password) {
-            $('.regMsg').html('Kindly fill all fields!');
-            return;
+        const email = $('#email').val();
+        //Check if user input is empty
+        if (!firstname || !lastname || !username || !password || !email) {
+        $('.regMsg').html('Kindly fill in all fields');
+        return;
         }
+        //Make get request to check if the user already exist
         $.ajax({
-            method: 'GET',
-            url: `http://localhost:3000/users?email=${email}`,
-            data: {
+        method: 'GET',
+        url: `http://localhost:3000/users?email=${email}`,
+        data: {
+            email,
+        },
+        beforeSend: function() {
+            $('.regMsg').html('Loading....');
+        },
+        success: function(response) {
+            if (response.length) {
+            $('.regMsg').html('User already exist');
+            } else {
+            
+            $.ajax({
+                method: 'POST',
+                url: 'http://localhost:3000/users',
+                data: {
+                firstname,
+                lastname,
                 username,
                 email,
-            },
-            success: function(response) {
-                if (response.length) {
-                    $('.regMsg').html('User already exists');
-                } else {
-                    $.ajax({
-                        method: 'POST',
-                        url: 'http://localhost:3000/users',
-                        data: {
-                            firstname,
-                            lastname,
-                            username,
-                            email,
-                            password,
-                        },
-                        success: function() {
-                            $('.regMsg').html('Registration Successful');
-                        },
-                    });
-                }
-            },
+                password,
+                },
+                beforeSend: function() {
+                $('.regMsg').html('Loading....');
+                },
+                success: function() {
+                $('.regMsg').html('Registration Successfull');
+                },
+            });
+            }
+        },
         });
     });
 });
