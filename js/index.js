5cee9bf5fdcf5b681661b1cf55ad1409655931c8
tharqvader <udokwu.chika@gmail.com>
2019-09-17 14:09:17 +0100

    testing and editing the signin form to the db

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Registration Part!
     $('.submitRegBtn').click(function(event) {
         event.preventDefault();
         const firstname = $('#firstname').val();
@@ -47,4 +48,45 @@ $(document).ready(function() {
         },
         });
     });
+
+    //Login Part!
+    $('.submitLogBtn').click(function(event) {
+        event.preventDefault();
+        const passwordLog = $('#passwordLog').val();
+        const emailLog = $('#emailLog').val();
+        if (!passwordLog || !emailLog) {
+          $('.regMsg').html('Kindly fill in all fields');
+          return;
+        }
+        //Check if the user is in the database
+        $.ajax({
+          method: 'GET',
+          url: `http://localhost:3000/users?email=${emailLog}&password=${passwordLog}`,
+          data: {
+            email: emailLog,
+            password: passwordLog,
+          },
+          beforeSend: function() {
+            $('.regMsg').html('Loading....');
+          },
+          success: function(response) {
+            if (response.length) {
+              $('.regMsg').html('Login sucessful');
+              $('.logChk').html('You are logged in');
+              localStorage.setItem('email', emailLog);
+              //redirect to home page if the login is successfull
+              window.location = 'index.html';
+            } else {
+              $('.regMsg').html('Username or password Incorrect');
+            }
+          },
+        });
+      });
+      //Logout Function
+      /*$('.logoutBtn').click(function() {
+        //clear the localstorage and redirect to signup page
+        localStorage.clear();
+        $('.logChk').html('Kindly login');
+        window.location.assign('signup.html');
+      });*/
 });
